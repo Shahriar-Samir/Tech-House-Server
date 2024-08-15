b95fb600919f45287355e80d8cbafd5d9b669a81
Shahriar-Samir <shabusiness035@gmail.com>
2024-08-15 09:18:38 +0600

    add: created a server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/',(req,res)=>{
+    res.send('Tech House server')
+})
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
